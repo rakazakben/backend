0aeaa723b69223d62825f451cd18d1ba5273a3e0
rakazak <rakazakben@gmail.com>
2025-03-27 10:36:31 +0100

    controller auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const user = require('../models/user');
 
+
 const router = express.Router();
+const userController = require('../controllers/auth');
 
-router.post(`/signup`, (req, res, next) => {
-    
-    next();
-  });
-router.post(`/login`, (req, res, next) => {
-    
-    next();
-  });
+router.post(`/signup`, userController.signup );
+router.post(`/login`, userController.login);
 
-  module.exports = router;
+module.exports = router;
