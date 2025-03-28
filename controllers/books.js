c0c93a1c4d2def12728a5329609ac0e3362b1d50
rakazak <rakazakben@gmail.com>
2025-03-28 12:04:12 +0100

    mise à jour editBook

--- controllers/books.js
+++ controllers/books.js
@@ -36,9 +36,29 @@ exports.editBook =  (req, res) => {
     if (!req.body) {
         return res.status(400).json({ message: " Aucune donnée envoyée." });
     }
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+
+    delete bookObject.userId;
+    Book.findOne({_id: req.params.id})
+       .then((book) => {
+           if (book.userId != req.auth.userId) {
+               res.status(401).json({ message : 'Not authorized'});
+           } else {
+               Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
+               .then(() => res.status(200).json({message : 'livre modifié!'}))
+               .catch(error => res.status(401).json({ error }));
+           }
+       })
+       .catch((error) => {
+           res.status(400).json({ error });
+       });
+    /*
     Book.updateOne({ _id: req.params.id }, { ...req.body })
     .then(() => res.status(200).json({ message: 'livre modifié !'}))
-    .catch(error => res.status(400).json({ error }));
+    .catch(error => res.status(400).json({ error }));*/
   };
 
 exports.deleteBook = (req, res) => {
